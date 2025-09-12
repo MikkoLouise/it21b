f075fe7d0e51939848b6761a074a9fdcfc94b22e
MikkoLouise <louisemikko11@gmail.com>
2025-09-12 08:25:28 +0800

    Added Class Person, Initial Display Info and Initial Constructor

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("This is app.js");
+class Person {
+    name;
+    age;
+    occupation;
+    message;
+
+    constructor(name, age, occupation, message){
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.message = message;
+    }
+
+    displayInfo(){
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Occupation: " + this.occupation);
+        console.log("Message: " + this.message);
+        console.log("-----------------------");
+
+}
+showName(){
+    console.log(this.name);
+}
+
+showMessage(){
+    console.log(this.message);
+}
+}
+const person1 = new Person("Coco Martin" , 30, "Web Developer", "Ser, I love coding!");
+const person2 = new Person("Victor Magtanggol" , 30, "Superman", "Darnaa!!!");
+
+person1.displayInfo();
+person2.displayInfo();
+
